8a2e6bfd28064b57d5fb08d4fcce1033bdcc21b5
Samuel Suesca Ríos <96037106+Samsuesca@users.noreply.github.com>
2023-09-08 00:23:32 -0500

    gool

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+
+st.set_page_config(page_title='Presentación del Ensayo Argumentativo',
+                    page_icon=':book:',
+                    layout='centered')
+
+
+
+
+st.title(f"Influencia del enfoque estructuralista en el análisis de las dinámicas económicas y las desigualdades regionales en Colombia")
+st.write('---')
+st.write(f"Por Samuel Suesca y Sara Garcia")
+st.write('---')
+st.title(f"¿Cómo ha influido la perspectiva estructuralista de la CEPAL, con énfasis en la teoría centro-periferia, en el análisis y la comprensión de la economia colombiana?")
+st.write('---')
+st.title(f"Introducción o contextualización del tema:")
+
+st.write('''El enfoque estructuralista y la teoría centro-periferia, desarrollados en principio por la Comisión Económica 
+         para América Latina y el Caribe (CEPAL) a lo largo del siglo pasado, han sido fundamentales en el análisis de las dinámicas 
+         económicas y las desigualdades regionales en Colombia (1), pero también en América Latina. Estas perspectivas han permitido comprender
+           las relaciones de poder, los desequilibrios económicos y las políticas públicas implementadas en
+             el país a lo largo de la historia. En este trabajo por tanto, se examinará el papel de estos puntos de vista en el análisis de las principales problemáticas y planteamientos económicos que han abordado el país; viendolo desde
+                el punto de vista de la Colombia interior (con sus regiones internas), pero también la Colombia regional, que pertence a latinoamerica (1) (2) (4).''')
+
+st.write(''' El enfoque estructuralista se basa en la idea de que las estructuras económicas y sociales determinan
+ el desarrollo de los países. Según esta perspectiva, el estudio de las economías latinoamericanas se debe basar en el análisis 
+  profundo de las dinámicas y desafios de cada economía, es decir, debe analizarse en el contexto de las estructuras económicas más amplias de cada país. 
+   Dentro de esta corriente, surge un modelo teórico conocido como centro-periferia, que plantea que latinoamerica
+     se encuentran en una posición periférica en el sistema económico global,
+   lo que las coloca en una situación de dependencia económica respecto a los países centrales (3).
+     Esta dependencia se manifiesta en la exportación de materias primas y productos de baja tecnología, 
+     mientras que las importaciones de bienes manufacturados y tecnología provienen de los países centrales (2).
+Esta dinámica ha llevado a la concentración de la actividad económica en las regiones centrales, generando desigualdades regionales significativas. 
+Trataremos de estudiar más el caso puntual de este enfoque en la economía e historia colombiana (3) (5) (6).''')
+         
+
+st.write('---')
+
+st.title(f"Hipótesis:")
+
+st.write('''Se plantea que el enfoque estructuralista y la teoría centro-periferia, tal como se evidencia en los documentos estudiados hasta ahora,
+          han influido significativamente en la historia económica del país, y han jugado un rol importante en el análisis de las problemáticas 
+           que han atravesado el horizonte económico de Colombia (3) (2).
+             Estas perspectivas, entre otras cosas, han permitido identificar las relaciones de dependencia económica con otros paises (pero también dentro de la dinámica interna),
+               la heterogeneidad estructural y la necesidad de políticas públicas orientadas a reducir las desigualdades regionales dentro del país, y en el caso de 
+               latinoamerica.''')
+st.write('---')
+st.title(f"Referencias de la revisión de literatura:")
+st.write('1: Desarrollo e igualdad: el pensamiento de la CEPAL en su séptimo decenio: https://www.cepal.org/es/publicaciones/43540-desarrollo-igualdad-pensamiento-la-cepal-su-septimo-decenio-textos-seleccionados')
+st.write('2: IDEAS DEL ESTRUCTURALISMO LATINOAMERICANO EN EL DISCURSO DE ALBERTO LLERAS CAMARGO')
+st.write('3: Análisis espacial de las disparidades regionales en Colombia: una aproximación desde la estructura centro periferia: http://hdl.handle.net/10893/21606 ')
+st.write('4: Lógica del estructuralismo latinoamericano en Colombia. Análisis del déficit en transporte marítimo')
+st.write('5: POLÍTICA Y CRECIMIENTO ECONÓMICO EN COLOMBIA, 1906-2009')
+st.write('6: Historia Económica de Colombia - Jose Antonio Ocampo')
+st.write('')
+st.write('---')
+
+
+# Agregar más contenido o diseño según tus necesidades
